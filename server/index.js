8e21b01086b3c72c4247db1e50dad45c9131c102
uruta <waltergagate443@gmail.com>
2024-05-30 21:42:15 +0800

    fix(cors): Added cors methods

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(cors({
     credentials: true, 
-    origin: `${process.env.CLIENT_DOMAIN}`, 
+    origin: `${process.env.CLIENT_DOMAIN}`,     
     methods: ['POST', 'GET']
 }))
 
